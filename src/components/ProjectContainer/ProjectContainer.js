d694023e962476cc716a80e7a7bf57f759355c95
malajvan <vanhongpham01@gmail.com>
2023-09-17 14:02:24 -0400

    initial commit

---
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -0,0 +1,48 @@
+import uniqid from 'uniqid'
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LaunchIcon from '@material-ui/icons/Launch'
+import './ProjectContainer.css'
+
+
+const ProjectContainer = ({ project }) => (
+  <div className='project'>
+    <h3>{project.name}</h3>
+    <img
+      src={project.image} // Assuming 'project.image' contains the URL of the image
+      alt={project.name}// You can set an appropriate alt text
+      className='project__image'
+    />
+    <p className='project__description'>{project.description}</p>
+    {project.stack && (
+      <ul className='project__stack'>
+        {project.stack.map((item) => (
+          <li key={uniqid()} className='project__stack-item'>
+            {item}
+          </li>
+        ))}
+      </ul>
+    )}
+
+    {project.sourceCode && (
+      <a
+        href={project.sourceCode}
+        aria-label='source code'
+        className='link link--icon'
+      >
+        <GitHubIcon />
+      </a>
+    )}
+
+    {project.livePreview && (
+      <a
+        href={project.livePreview}
+        aria-label='live preview'
+        className='link link--icon'
+      >
+        <LaunchIcon />
+      </a>
+    )}
+  </div>
+)
+
+export default ProjectContainer
